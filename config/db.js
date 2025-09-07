2a7d01d7a6e6db679d3cdbe23aa691996e66b746
ishoef <ismailhossennayeb@gmail.com>
2025-09-07 10:29:39 +0600

    some update

--- config/db.js
+++ config/db.js
@@ -4,6 +4,8 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`📂 Using Database: ${conn.connection.name}`);
+
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
     process.exit(1); // Exit process if connection fails
